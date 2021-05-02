c4b0add6f0514030e1a94cdb155523688d5caa17
nadhifikbarw <nadhifikbarw@gmail.com>
2021-05-02 11:36:50 +0700

    Update check_shacl script

--- check_shacl.sh
+++ check_shacl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-DATAGRAPH='./datagraph_copy.ttl'
+DATAGRAPH='./datagraph.ttl'
 echo "Testing resource datagraph: ${DATAGRAPH}" > report.txt
 python3 -m pyshacl -s ./shacl.ttl -df turtle -ef turtle -sf turtle -f human $DATAGRAPH >> report.txt
